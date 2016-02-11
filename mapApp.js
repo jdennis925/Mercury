3041663021579339e9f0c15c9cd29e8b6c6feee7
John <jdennis925@gmail.com>
2016-02-10 19:09:57 -0600

    Terrain Buttons

--- mapApp.js
+++ mapApp.js
@@ -1,7 +1,7 @@
-
 var $ = function(id){
 	return document.getElementById(id);
 }
+
 var AddOneButton_Click = function()
 {
 	var cur2 = parseFloat($("AddOneButton").value);
@@ -17,17 +17,17 @@ var AddOneButton_Click = function()
     
 	var y = document.createElement("TR");
     y.setAttribute("id", "myTr"+ cur);
-    document.getElementById("myTable").appendChild(y);
+    $("myTable").appendChild(y);
 
     var z = document.createElement("TD");
     z.setAttribute("id", "myTd"+ cur);
     
     var t = document.createTextNode("cell");
     z.appendChild(t);
-    document.getElementById(y.id).appendChild(z);
+    $(y.id).appendChild(z);
 }
 
-var MapMap_Click = function()
+var MakeMap_Click = function()
 {
     if($("mapTable"))
     {
@@ -40,26 +40,42 @@ var MapMap_Click = function()
 	var rowCount = parseInt($("txtRowCount").value);
 	var colCount = parseInt($("txtColCount").value);
 	
+    for (var colIndex = 0; colIndex < colCount; colIndex++) 
+    {
+        var col = document.createElement("col");
+        col.setAttribute("width", "100");
+        $(mapTable.id).appendChild(col);      
+    }
+    
     for(var rowIndex = 0; rowIndex < rowCount; rowIndex++)
     {
         var row = document.createElement("TR");
         row.setAttribute("id", "row_" + rowIndex);
-        document.getElementById(mapTable.id).appendChild(row);
+        $(mapTable.id).appendChild(row);
+        row.setAttribute("height", "100");
+        
 	
         for(var colIndex = 0; colIndex < colCount; colIndex++)
         {
             var col = document.createElement("TD");
-            col.setAttribute("id", "col_" + colIndex);
-    
-            var text = document.createTextNode("X");
-            col.appendChild(text);
-            document.getElementById(row.id).appendChild(col);        
+            var colId = "cell_" + rowIndex + "_" + colIndex;
+            col.setAttribute("id", colId);
+            $(row.id).appendChild(col);
+            $(colId).onclick = btnImpassableTerrain_Click;
         }
     }
 }
 
+var btnImpassableTerrain_Click = function()
+{
+    var cell = $("cell_2_2");
+    cell.setAttribute("bgcolor", "#FFFF00")
+    var text = document.createTextNode("X");
+    cell.appendChild(text);
+}
+
 window.onload = function()
 {
-	$("AddOneButton").onclick = AddOneButton_Click;
-	$("btnMakeMap").onclick = MapMap_Click;
+	$("btnMakeMap").onclick = MakeMap_Click;
+	// $("btnImpassableTerrain").onclick = btnImpassableTerrain_Click;
 }
